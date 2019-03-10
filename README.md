50f192ba7b79bd48a87d3f7c08429068493804e9
johnSickels <46609057+johnsickels@users.noreply.github.com>
2019-03-10 06:35:29 -0400

    psuedocode js

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Psychic-Game
+
+Guess what letter the computer has chosen!
+
+Hit any key to begin. Score is kept track. 
---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// app randomly picks random letter
+
+// user strikes key: userChoice
+
+//if appChoice = userChoice: win, 
+//increase wins, 
+//restart game
+
+//else: lose, 
+//return character to guesses so far, 
+//decrease guesses left, 
+//restart game
