8d9f7e1fb8e360f39e9fa91dc7f1f0ca92dc29a6
johnSickels <46609057+johnsickels@users.noreply.github.com>
2019-03-13 11:47:14 -0400

    choice submit working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,36 @@
 // wins = 0
+var win = 0
 // loses = 0
+var lose = 0
 // guesses left = 9
+var guessesLeft = 9
 // characters available
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 // characters guessed
+var guesses = []
 
 
 // app randomly picks random letter
+var appChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+console.log(appChoice);
 // user keys character
+document.onkeyup = function(event) {
+  userChoice = event.key.toLowerCase();
+  console.log(userChoice);
 
-//if appChoice = userChoice: win, 
-//increase wins, 
-//restart game
-
-//else: lose, 
-//return character to guesses so far, 
-//decrease guesses left, 
-//restart game
+  //if appChoice = userChoice: win, 
+  //increase wins, 
+  //restart game
+  if (appChoice === userChoice) {
+    alert("Correct! You are such a good guesser");
+    win++;
+    //restart
+  } else {
+    lose--;
+    // restart
+    //else: lose, 
+  //return character to guesses so far, 
+  //decrease guesses left, 
+  //restart game
+  }
+}
