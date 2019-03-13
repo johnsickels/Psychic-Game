79508c7d34084de2d02eba9fdbe3dd28d308b6f9
johnSickels <46609057+johnsickels@users.noreply.github.com>
2019-03-13 14:35:05 -0400

    finish javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,65 @@
-// wins = 0
-var win = 0
-// loses = 0
-var lose = 0
-// guesses left = 9
-var guessesLeft = 9
-// characters available
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-// characters guessed
-var guesses = []
+// global variables
 
+var win = 0;
+console.log ("wins = " + win);
+var lose = 0;
+console.log ("loses = " + lose);
+var guessesLeft = 9;
+console.log ("guesses left = " + guessesLeft);
+var guesses = [];
+var userChoice = "";
+var appChoice = "";
+
+var userGuesses = document.getElementById("#guesses");
+
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // app randomly picks random letter
-var appChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-console.log(appChoice);
+
+function psychic () {
+  guessesLeft = 9;
+  guesses = [];
+  appChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+  console.log("psychic thinks of = " + appChoice);
+}
+alert ("Try to guess what letter the psychic is thinking!");
+psychic ();
+
 // user keys character
+
 document.onkeyup = function(event) {
   userChoice = event.key.toLowerCase();
   console.log(userChoice);
 
   //if appChoice = userChoice: win, 
   //increase wins, 
-  //restart game
-  if (appChoice === userChoice) {
+  if (!/[a-z]/.test(userChoice)){
+		alert ("please choose a letter");
+  } else if (userChoice === "meta") {
+    alert ("Try to guess what letter the psychic is thinking!");
+  } else if (appChoice === userChoice) {
     alert("Correct! You are such a good guesser");
     win++;
     //restart
-  } else {
-    lose--;
-    // restart
+    psychic ();
+  } else if (guessesLeft === 1) {
     //else: lose, 
-  //return character to guesses so far, 
-  //decrease guesses left, 
-  //restart game
+    guessesLeft--;
+    alert("You lose! Try again.");
+    lose++;
+    //restart game
+    psychic();
+  } else {
+    //decrease guesses left, 
+    guessesLeft--;
+    console.log("guesses left = " + guessesLeft);
+    guesses.push(userChoice);
+
   }
+
+  // Send variables to html
+  document.querySelector("#win").innerHTML = "Wins : " + win;
+  document.querySelector("#lose").innerHTML = "Losses : " + lose;
+  document.querySelector("#guessesLeft").innerHTML = "Guesses left : " + guessesLeft;
+  document.querySelector("#guesses").innerHTML = "Your guesses so far : " + guesses ;
 }
