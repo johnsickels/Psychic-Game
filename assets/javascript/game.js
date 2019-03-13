f9693f03daa38705e0bedff7c724103f7996610e
johnSickels <46609057+johnsickels@users.noreply.github.com>
2019-03-13 18:25:14 -0400

    polish style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
 // global variables
 
 var win = 0;
-console.log ("wins = " + win);
+console.log("wins = " + win);
 var lose = 0;
-console.log ("loses = " + lose);
+console.log("loses = " + lose);
 var guessesLeft = 9;
-console.log ("guesses left = " + guessesLeft);
+console.log("guesses left = " + guessesLeft);
 var guesses = [];
 var userChoice = "";
 var appChoice = "";
@@ -16,29 +16,29 @@ var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 
 // app randomly picks random letter
 
-function psychic () {
+function psychic() {
   guessesLeft = 9;
   guesses = [];
   appChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
   console.log("psychic thinks of = " + appChoice);
 }
-psychic ();
+psychic();
 
 // user keys character
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
   userChoice = event.key.toLowerCase();
   console.log("user guesses = " + userChoice);
 
   //if appChoice = userChoice: win, 
   //increase wins, 
   if (!/[a-z]/.test(userChoice) || userChoice === "meta" || userChoice === "shift" || userChoice === "enter" || userChoice === "arrowright" || userChoice === "arrowleft" || userChoice === "arrowup" || userChoice === "arrowdown" || userChoice === "backspace" || userChoice === "alt" || userChoice === "control" || userChoice === "escape") {
-    alert ("Please choose a character a though z.");
+    alert("Please choose a character a though z.");
   } else if (appChoice === userChoice) {
-    alert("Correct! You are such a good guesser. The psychic was thinking of the letter " + appChoice);
+    alert("Correct! You are such a good guesser. The psychic was thinking of the letter " + appChoice + ".");
     win++;
     //restart
-    psychic ();
+    psychic();
   } else if (guessesLeft === 1) {
     //else: lose, 
     guessesLeft--;
@@ -58,5 +58,5 @@ document.onkeyup = function(event) {
   document.querySelector("#win").innerHTML = "Wins : " + win;
   document.querySelector("#lose").innerHTML = "Losses : " + lose;
   document.querySelector("#guessesLeft").innerHTML = "Guesses left : " + guessesLeft;
-  document.querySelector("#guesses").innerHTML = "Your guesses so far : " + guesses ;
+  document.querySelector("#guesses").innerHTML = "Your guesses so far : " + guesses;
 }
